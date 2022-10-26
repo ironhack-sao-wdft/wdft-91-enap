9dc686caef3b07fe28b1828f9f8559f36d51c824
tatialveso <tatiialveso@gmail.com>
2022-10-26 11:30:12 -0300

    day 3 week 2

---
+++ javascript/calculator/script.js
@@ -0,0 +1,37 @@
+class Calculator {
+    sum = (num1, num2) => {
+        return Number(num1) + Number(num2)
+    }
+
+    sub = (num1, num2) => {
+        return num1 - num2
+    }
+}
+
+// criando o meu objeto
+const calculadora = new Calculator()
+// calculadora.sum(num1, num2)
+
+// passo 1: identificar todos os elementos html que vão ser manipulados
+const numberOne = document.getElementById("numberOne")
+const numberTwo = document.querySelector("#numberTwo")
+const result = document.getElementById("result")
+const resultBtn = document.querySelector("#resultBtn")
+
+//console.log(numberOne)
+
+// eventos -> desencadear um acontecimento
+// addEventListener(o que precisa "ouvir", callback)
+    // -> adicionar um evento a partir daquilo que ele "ouvir"
+resultBtn.addEventListener("click", () => {
+
+    let inputOne = numberOne.value
+    console.log(inputOne)
+    let inputTwo = numberTwo.value
+    console.log(inputTwo)
+
+    let resultCalc = calculadora.sum(inputOne, inputTwo)
+    console.log(resultCalc)
+
+    result.innerText = resultCalc
+})
